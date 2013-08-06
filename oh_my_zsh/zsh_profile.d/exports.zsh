8c92e47ec5eb372244a36e97083f95dedb833ef0
B. Ruschill <brandon.ruschill@gmail.com>
2013-08-06 14:13:30 -0500

    remove vagrant log export from zsh exports config

--- oh_my_zsh/zsh_profile.d/exports.zsh
+++ oh_my_zsh/zsh_profile.d/exports.zsh
@@ -3,5 +3,3 @@ export LC_CTYPE="utf-8"
 export SSL_CERT_FILE=/usr/local/opt/curl-ca-bundle/share/ca-bundle.crt
 
 export NODE_PATH=/usr/local/share/npm/bin:/usr/local/share/npm/lib/node_modules
-
-export VAGRANT_LOG=error
