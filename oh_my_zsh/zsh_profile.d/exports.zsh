01980cf2720ac9240aec6e93b1161c14be7c2dff
B. Ruschill <brandon.ruschill@gmail.com>
2013-09-08 09:33:53 -0500

    set up Go config

--- oh_my_zsh/zsh_profile.d/exports.zsh
+++ oh_my_zsh/zsh_profile.d/exports.zsh
@@ -3,3 +3,5 @@ export LC_CTYPE="utf-8"
 export SSL_CERT_FILE=/usr/local/opt/curl-ca-bundle/share/ca-bundle.crt
 
 export NODE_PATH=/usr/local/share/npm/bin:/usr/local/share/npm/lib/node_modules
+
+export GOPATH=$HOME/Documents/code/go
