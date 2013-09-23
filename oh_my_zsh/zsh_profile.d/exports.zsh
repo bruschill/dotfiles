173a21037ea4e524b1e2e4c414bec0f72141b562
B. Ruschill <brandon.ruschill@gmail.com>
2013-09-23 18:39:48 -0500

    add ruby-specific exports

--- oh_my_zsh/zsh_profile.d/exports.zsh
+++ oh_my_zsh/zsh_profile.d/exports.zsh
@@ -5,3 +5,6 @@ export SSL_CERT_FILE=/usr/local/opt/curl-ca-bundle/share/ca-bundle.crt
 export NODE_PATH=/usr/local/share/npm/bin:/usr/local/share/npm/lib/node_modules
 
 export GOPATH=$HOME/Documents/code/go
+
+export RUBY_GC_MALLOC_LIMIT=90000000
+export RUBY_FREE_MIN=200000
