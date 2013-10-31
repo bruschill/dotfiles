bbe3ff31176f1b31a8a2ebf82276fbb67c2d4e2a
B. Ruschill <brandon.ruschill@gmail.com>
2013-10-31 11:47:22 -0700

    remove everything rvm related

--- oh_my_zsh/zsh_profile.d/exports.zsh
+++ oh_my_zsh/zsh_profile.d/exports.zsh
@@ -4,5 +4,7 @@ export SSL_CERT_FILE=/usr/local/opt/curl-ca-bundle/share/ca-bundle.crt
 
 export NODE_PATH=/usr/local/share/npm/bin:/usr/local/share/npm/lib/node_modules
 
+export RBENV_VERSION=2.0.0-p247
+
 export RUBY_GC_MALLOC_LIMIT=90000000
 export RUBY_FREE_MIN=200000
