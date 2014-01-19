3d769c67daa4b10bc6f277a3ba314532372d0864
B. Ruschill <brandon.ruschill@gmail.com>
2014-01-18 17:03:38 -0800

    update exports.zsh
    - change RBENV_VERSION from 2.0.0-p353 to 2.1.0
    - change RUBY_FREE_MIN to RUBYGC_HEAP_FREE_SLOTS

--- oh_my_zsh/zsh_profile.d/exports.zsh
+++ oh_my_zsh/zsh_profile.d/exports.zsh
@@ -4,10 +4,10 @@ export SSL_CERT_FILE=/usr/local/opt/curl-ca-bundle/share/ca-bundle.crt
 
 export NODE_PATH=/usr/local/share/npm/bin:/usr/local/share/npm/lib/node_modules
 
-export RBENV_VERSION=2.0.0-p353
+export RBENV_VERSION=2.1.0
 
 export RUBY_GC_MALLOC_LIMIT=90000000
-export RUBY_FREE_MIN=200000
+export RUBYGC_HEAP_FREE_SLOTS=200000
 
 export ARCHFLAGS="-arch x86_64"
 
