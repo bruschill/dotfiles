a729dec43ad7c9f83927c3d44a3f3e69dda37b8b
B. Ruschill <brandon.ruschill@gmail.com>
2014-02-25 14:12:05 -0800

    remove RBENV_VERSION from exports

--- oh_my_zsh/zsh_profile.d/exports.zsh
+++ oh_my_zsh/zsh_profile.d/exports.zsh
@@ -4,8 +4,6 @@ export SSL_CERT_FILE=/usr/local/opt/curl-ca-bundle/share/ca-bundle.crt
 
 export NODE_PATH=/usr/local/share/npm/bin:/usr/local/share/npm/lib/node_modules
 
-export RBENV_VERSION=2.1.0
-
 export RUBY_GC_MALLOC_LIMIT=90000000
 export RUBYGC_HEAP_FREE_SLOTS=200000
 
