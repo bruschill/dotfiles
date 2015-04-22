b8578c2389015514bdad11b701c3a66a4697c7ab
B. Ruschill <brandon.ruschill@gmail.com>
2015-04-22 09:37:37 -0700

    add new relic project dir to auto complete

--- oh_my_zsh/zsh_profile.d/navigation.zsh
+++ oh_my_zsh/zsh_profile.d/navigation.zsh
@@ -1,4 +1,4 @@
-cdpath=(. $HOME/Documents/code $HOME/Documents/code/go/src)
+cdpath=(. $HOME/Documents/code $HOME/Documents/code/go/src $HOME/Documents/code/new_relic)
 
 current() {
   if [[ -f $CURRENT_PROJECT_PATH ]]; then
