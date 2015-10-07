925a25d60925f1c38c701ebe78f1a1d35905de4e
B. Ruschill <brandon.ruschill@gmail.com>
2015-10-07 06:40:08 -0700

    add binding for fzf-history-widget

--- oh_my_zsh/zsh_profile.d/fzf.zsh
+++ oh_my_zsh/zsh_profile.d/fzf.zsh
@@ -1,4 +1,9 @@
-# history completion
-fh() {
-  print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
-}
+export FZF_DEFAULT_OPTS='
+--extended
+--bind ctrl-f:page-down,ctrl-b:page-up
+--color fg:-1,bg:-1,hl:166,fg+:234,bg+:247,hl+:234
+--color info:-1,prompt:254,spinner:166,pointer:234,marker:247
+'
+
+bindkey '^P' fzf-file-widget
+bindkey '^F' fzf-history-widget
