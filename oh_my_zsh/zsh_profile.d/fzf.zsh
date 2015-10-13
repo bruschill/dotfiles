508845f576fce1bfde672e5d819da6b95b943e06
B. Ruschill <brandon.ruschill@gmail.com>
2015-10-13 00:13:50 -0700

    update fzf config

--- oh_my_zsh/zsh_profile.d/fzf.zsh
+++ oh_my_zsh/zsh_profile.d/fzf.zsh
@@ -1,6 +1,9 @@
+export FZF_DEFAULT_COMMAND='ag -l -g ""'
+
 export FZF_DEFAULT_OPTS='
 --extended
 --bind ctrl-f:page-down,ctrl-b:page-up
+--ansi
 --color fg:-1,bg:-1,hl:166,fg+:234,bg+:247,hl+:234
 --color info:-1,prompt:254,spinner:166,pointer:234,marker:247
 '
