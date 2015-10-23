5f1342d706dac4fbca50cb6ce9fa2587b11d5301
B. Ruschill <brandon.ruschill@gmail.com>
2015-10-23 00:42:08 -0700

    make fzf respect git, hg, and svn ignore files

--- oh_my_zsh/zsh_profile.d/fzf.zsh
+++ oh_my_zsh/zsh_profile.d/fzf.zsh
@@ -1,7 +1,8 @@
 export FZF_DEFAULT_COMMAND='ag -l -g ""'
 
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
 export FZF_DEFAULT_OPTS='
---extended
 --bind ctrl-f:page-down,ctrl-b:page-up
 --ansi
 --color fg:-1,bg:-1,hl:166,fg+:234,bg+:247,hl+:234
