0bf4b347961f2e9cf017f7c3ffc54ccef3275c5c
B. Ruschill <brandon.ruschill@gmail.com>
2015-10-23 00:57:16 -0700

    small tweak to fzf colors

--- oh_my_zsh/zsh_profile.d/fzf.zsh
+++ oh_my_zsh/zsh_profile.d/fzf.zsh
@@ -5,8 +5,8 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_DEFAULT_OPTS='
 --bind ctrl-f:page-down,ctrl-b:page-up
 --ansi
---color fg:-1,bg:-1,hl:166,fg+:234,bg+:247,hl+:234
---color info:-1,prompt:254,spinner:166,pointer:234,marker:247
+--color fg:-1,bg:-1,hl:160,fg+:234,bg+:247,hl+:234
+--color info:-1,prompt:254,spinner:160,pointer:234,marker:247
 '
 
 bindkey '^P' fzf-file-widget
