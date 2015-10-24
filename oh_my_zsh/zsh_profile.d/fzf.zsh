f75942776d231567a28f31e256a6d1d21d8f6981
B. Ruschill <brandon.ruschill@gmail.com>
2015-10-23 19:34:30 -0700

    MORE FZF CONFIG

--- oh_my_zsh/zsh_profile.d/fzf.zsh
+++ oh_my_zsh/zsh_profile.d/fzf.zsh
@@ -1,4 +1,4 @@
-export FZF_DEFAULT_COMMAND='ag -l -g ""'
+export FZF_DEFAULT_COMMAND=' (git ls-tree -r --name-only HEAD || ag -l -g "") 2> /dev/null'
 
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
@@ -12,3 +12,41 @@ export FZF_DEFAULT_OPTS='
 bindkey '^P' fzf-file-widget
 bindkey '^H' fzf-history-widget
 bindkey '^D' fzf-cd-widget
+
+# HELPER FUNCTIONS
+# open matched file in editor of choice
+fe() {
+  local file
+  file=$(fzf-tmux --query="$1" --select-1 --exit-0)
+  [ -n "$file" ] && ${EDITOR:-vim} "$file"
+}
+
+# checkout local/remote git branch
+fbr() {
+  local branches branch
+  branches=$(git branch --all | grep -v HEAD) &&
+  branch=$(echo "$branches" | fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
+
+# easier git stash management
+fstash() {
+  local out q k sha
+  while out=$(
+    git stash list --pretty="%C(yellow)%h %>(14)%Cgreen%cr %C(blue)%gs" |
+    fzf-tmux --ansi --no-sort --query="$q" --print-query --expect=ctrl-d,ctrl-b);
+  do
+    q=$(head -1 <<< "$out")
+    k=$(head -2 <<< "$out" | tail -1)
+    sha=$(tail -1 <<< "$out" | cut -d' ' -f1)
+    [ -z "$sha" ] && continue
+    if [ "$k" = 'ctrl-d' ]; then
+      git diff $sha
+    elif [ "$k" = 'ctrl-b' ]; then
+      git stash branch "stash-$sha" $sha
+      break;
+    else
+      git stash show -p $sha
+    fi
+  done
+}
