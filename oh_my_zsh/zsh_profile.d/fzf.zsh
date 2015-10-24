ec1bf8ce37c9778296a8e65aae467e50cdbebd59
B. Ruschill <brandon.ruschill@gmail.com>
2015-10-24 00:15:27 -0700

    fix the fzf default command

--- oh_my_zsh/zsh_profile.d/fzf.zsh
+++ oh_my_zsh/zsh_profile.d/fzf.zsh
@@ -1,4 +1,4 @@
-export FZF_DEFAULT_COMMAND=' (git ls-tree -r --name-only HEAD || ag -l -g "") 2> /dev/null'
+export FZF_DEFAULT_COMMAND='ag -l -g ""'
 
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
