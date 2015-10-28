354454078cba66e7435b30054e0311892c497dbb
B. Ruschill <brandon.ruschill@gmail.com>
2015-10-27 22:09:47 -0700

    update fzf config

--- zsh/zsh_config.d/fzf.zsh
+++ zsh/zsh_config.d/fzf.zsh
@@ -1,3 +1,17 @@
+# INITIALIZATION
+if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+  export PATH="$PATH:/usr/local/opt/fzf/bin"
+fi
+
+if [[ ! "$MANPATH" == */usr/local/opt/fzf/man* && -d "/usr/local/opt/fzf/man" ]]; then
+  export MANPATH="$MANPATH:/usr/local/opt/fzf/man"
+fi
+
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+
+# GENERAL
 export FZF_DEFAULT_COMMAND='ag -l -g ""'
 
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
@@ -9,6 +23,7 @@ export FZF_DEFAULT_OPTS='
 --color info:-1,prompt:254,spinner:160,pointer:234,marker:247
 '
 
+# KEY BINDINGS
 bindkey '^P' fzf-file-widget
 bindkey '^H' fzf-history-widget
 bindkey '^D' fzf-cd-widget
