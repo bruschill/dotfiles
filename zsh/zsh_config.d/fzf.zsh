d5aa692ce404bf95461eb9ff86c79e6f7c6901c6
B. Ruschill <brandon.ruschill@gmail.com>
2016-04-26 15:44:23 -0500

    remove unused commands

--- zsh/zsh_config.d/fzf.zsh
+++ zsh/zsh_config.d/fzf.zsh
@@ -59,25 +59,3 @@ fbr() {
   branch=$(echo "$branches" | fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
   git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
 }
-
-# easier git stash management
-fstash() {
-  local out q k sha
-  while out=$(
-    git stash list --pretty="%C(yellow)%h %>(14)%Cgreen%cr %C(blue)%gs" |
-    fzf-tmux --ansi --no-sort --query="$q" --print-query --expect=ctrl-d,ctrl-b);
-  do
-    q=$(head -1 <<< "$out")
-    k=$(head -2 <<< "$out" | tail -1)
-    sha=$(tail -1 <<< "$out" | cut -d' ' -f1)
-    [ -z "$sha" ] && continue
-    if [ "$k" = 'ctrl-d' ]; then
-      git diff $sha
-    elif [ "$k" = 'ctrl-b' ]; then
-      git stash branch "stash-$sha" $sha
-      break;
-    else
-      git stash show -p $sha
-    fi
-  done
-}
