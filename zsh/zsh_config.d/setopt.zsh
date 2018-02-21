e236b986d75c0dd4c7a4df730cf2e8bca5f7b8fb
B. Ruschill <brandon.ruschill@gmail.com>
2018-02-21 09:08:01 -0600

    remove duplicate history config

--- zsh/zsh_config.d/setopt.zsh
+++ zsh/zsh_config.d/setopt.zsh
@@ -5,17 +5,6 @@ setopt no_beep
 setopt auto_cd
 setopt pushd_ignore_dups
 
-# history
-setopt append_history
-setopt extended_history
-setopt inc_append_history
-setopt hist_expire_dups_first
-setopt hist_ignore_dups
-setopt hist_reduce_blanks
-setopt hist_ignore_space
-setopt hist_verify
-setopt share_history
-
 # completion
 setopt auto_name_dirs
 
