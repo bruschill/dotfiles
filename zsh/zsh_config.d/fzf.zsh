febeb3b2fcb8749fb24e645af0a2d35874108a2e
B. Ruschill <brandon.ruschill@gmail.com>
2019-06-28 16:14:12 -0500

    update some more files

--- zsh/zsh_config.d/fzf.zsh
+++ zsh/zsh_config.d/fzf.zsh
@@ -12,7 +12,7 @@ fi
 source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 
 # GENERAL
-export FZF_DEFAULT_COMMAND='rg --files --no-ignore-vcs --hidden'
+export FZF_DEFAULT_COMMAND='rg --files'
 
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
